7d2f9b94a3e92e7f1c50712c5ec03116b0894a02
Omowunmi <oekun@seamfix.com>
2022-07-09 10:20:58 +0100

    add deployment yaml

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a21332ead4717477ab595a19e9d346fc-1221879048.us-east-1.elb.amazonaws.com'
 };
 
 /*
